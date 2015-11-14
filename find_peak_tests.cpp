d7b21f97e1b98941e5b1038bef235d033b6c0294
QuentinDuval <senmenty@gmail.com>
2015-11-14 22:34:24 +0100

    Removing the rec impl

--- find_peak_tests.cpp
+++ find_peak_tests.cpp
@@ -12,8 +12,6 @@
 TEST(FindPeakTest, empty_vector)
 {
    std::vector<int> empty;
-   EXPECT_EQ(end(empty), find_peak_rec(empty));
-   EXPECT_EQ(end(empty), find_peak_rec(begin(empty), end(empty)));
    EXPECT_EQ(end(empty), find_peak(empty));
    EXPECT_EQ(end(empty), find_peak(begin(empty), end(empty)));
 }
@@ -21,21 +19,16 @@ TEST(FindPeakTest, empty_vector)
 TEST(FindPeakTest, filled_vector)
 {
    std::vector<int> seq{ 1, 2, 3, 2, 4, 5, 6 };
-   EXPECT_EQ(3, *find_peak_rec(seq));
    EXPECT_EQ(3, *find_peak(seq));
 }
 
 TEST(FindPeakTest, monotonous_vector)
 {
    std::vector<int> seq{ 1, 2, 3, 4, 5, 6, 7, 8 };
-   EXPECT_EQ(8, *find_peak_rec(seq));
    EXPECT_EQ(8, *find_peak(seq));
-
-   EXPECT_EQ(1, *find_peak_rec(seq, std::greater<int>()));
    EXPECT_EQ(1, *find_peak(seq, std::greater<int>()));
 
    std::reverse(begin(seq), end(seq));
-   EXPECT_EQ(8, *find_peak_rec(seq));
    EXPECT_EQ(8, *find_peak(seq));
 }
 
@@ -44,14 +37,12 @@ TEST(FindPeakTest, monotonous_vector)
 TEST(FindPeakTest, empty_list)
 {
    std::list<int> empty;
-   EXPECT_EQ(end(empty), find_peak_rec(empty));
    EXPECT_EQ(end(empty), find_peak(empty));
 }
 
 TEST(FindPeakTest, filled_list)
 {
    std::list<int> seq{ 1, 2, 3, 2, 4, 5, 6 };
-   EXPECT_EQ(3, *find_peak_rec(seq));
    EXPECT_EQ(3, *find_peak(seq));
 }
 
