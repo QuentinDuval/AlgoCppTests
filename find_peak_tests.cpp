6fd263053db198bca85f370da67dd8046fd5f36d
QuentinDuval <senmenty@gmail.com>
2015-11-14 02:51:06 +0100

    Tests for lists

--- find_peak_tests.cpp
+++ find_peak_tests.cpp
@@ -41,11 +41,25 @@ TEST(FindPeakTest, monotonous_vector)
 
 //-----------------------------------------------------------------------------
 
+TEST(FindPeakTest, empty_list)
+{
+   std::list<int> empty;
+   EXPECT_EQ(end(empty), find_peak_rec(empty));
+   EXPECT_EQ(end(empty), find_peak(empty));
+}
+
+TEST(FindPeakTest, filled_list)
+{
+   std::list<int> seq{ 1, 2, 3, 2, 4, 5, 6 };
+   EXPECT_EQ(3, *find_peak_rec(seq));
+   EXPECT_EQ(3, *find_peak(seq));
+}
+
 TEST(FindPeakTest, monotonous_list)
 {
    std::list<int> seq{ 1, 2, 3, 4, 5, 6, 7, 8 };
-   EXPECT_EQ(8, *find_peak_rec(seq));
    EXPECT_EQ(8, *find_peak(seq));
+   EXPECT_EQ(1, *find_peak(seq, std::greater<int>()));
 }
 
 //-----------------------------------------------------------------------------
