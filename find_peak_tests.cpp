2de1bd870dc9a83e50f141d4170ce0f203114a24
QuentinDuval <senmenty@gmail.com>
2015-11-14 01:38:27 +0100

    Find peak

--- find_peak_tests.cpp
+++ find_peak_tests.cpp
@@ -10,26 +10,24 @@
 TEST(FindPeakTest, empty_vector)
 {
    std::vector<int> empty;
-   auto it = find_peak(begin(empty), end(empty));
-   EXPECT_EQ(end(empty), it);
-   EXPECT_EQ(end(empty), find_peak(empty));
+   EXPECT_EQ(end(empty), find_peak_rec(empty));
+   EXPECT_EQ(end(empty), find_peak_rec(begin(empty), end(empty)));
 }
 
 TEST(FindPeakTest, filled_sequence)
 {
    std::vector<int> seq{ 1, 2, 3, 2, 4, 5, 6 };
-   auto it = find_peak(begin(seq), end(seq));
+   auto it = find_peak_rec(seq);
    EXPECT_EQ(3, *it);
-   EXPECT_EQ(it, find_peak(seq));
 }
 
 TEST(FindPeakTest, monotonous_sequence)
 {
    std::vector<int> seq{ 1, 2, 3, 4, 5, 6, 7, 8 };
-   auto it1 = find_peak(begin(seq), end(seq), std::less<int>());
+   auto it1 = find_peak_rec(seq);
    EXPECT_EQ(8, *it1);
 
-   auto it2 = find_peak(begin(seq), end(seq), std::greater<int>());
+   auto it2 = find_peak_rec(seq, std::greater<int>());
    EXPECT_EQ(1, *it2);
 }
 
