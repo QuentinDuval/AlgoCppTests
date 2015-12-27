7f90e14fe101b099451277920c1f5a20d29ccd3a
QuentinDuval <senmenty@gmail.com>
2015-12-27 14:15:27 +0100

    Improving tests (and use cases)

--- counting_sort_tests.cpp
+++ counting_sort_tests.cpp
@@ -17,5 +17,17 @@ TEST(CountingSortTest, string_by_length)
       EXPECT_EQ(expected[i], strings[i]);
 }
 
+TEST(CountingSortTest, string_with_associated_value)
+{
+   std::vector<std::string> strings    { "abcd", "ef", "ghi", "jklm", "" };
+   std::vector<size_t>      preferences{ 11,     1,     0,    10,     2  };
+   std::vector<std::string> expected   { "ghi", "ef", "", "jklm", "abcd" };
+
+   counting_sort(begin(strings), end(strings), begin(preferences), end(preferences));
+   ASSERT_EQ(expected.size(), strings.size());
+   for (size_t i = 0; i < expected.size(); ++i)
+      EXPECT_EQ(expected[i], strings[i]);
+}
+
 //-----------------------------------------------------------------------------
 
