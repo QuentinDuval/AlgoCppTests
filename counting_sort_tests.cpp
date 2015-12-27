77455aee882d267dbc88b0024be86aea2508bf5b
QuentinDuval <senmenty@gmail.com>
2015-12-27 14:19:53 +0100

    Add tests for signed ints

--- counting_sort_tests.cpp
+++ counting_sort_tests.cpp
@@ -20,7 +20,7 @@ TEST(CountingSortTest, string_by_length)
 TEST(CountingSortTest, string_with_associated_value)
 {
    std::vector<std::string> strings    { "abcd", "ef", "ghi", "jklm", "" };
-   std::vector<size_t>      preferences{ 11,     1,     0,    10,     2  };
+   std::vector<int>         preferences{ 11,     1,     -1,   10,     2  };
    std::vector<std::string> expected   { "ghi", "ef", "", "jklm", "abcd" };
 
    counting_sort(begin(strings), end(strings), begin(preferences), end(preferences));
