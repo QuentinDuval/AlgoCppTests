68a2abec0fe3e15a767a7a154fd82a69cd4e0165
QuentinDuval <senmenty@gmail.com>
2016-04-23 17:40:54 +0200

    Adding misses

--- hamt_tests.cpp
+++ hamt_tests.cpp
@@ -46,33 +46,63 @@ TEST(HamtTest, perf_no_collisions)
 {
    std::vector<std::pair<int, int>> inputs;
    hash_array_mapped_trie<int, int> hamt;
+   static const std::size_t SIZE = 1000000;
 
    show_time(std::cout, "1,000,000 int inserts in ms", 1, [&]()
    {
-      for (size_t i = 0; i < 1000000; ++i)
+      for (size_t i = 0; i < SIZE; ++i)
          hamt.insert(i, i);
    });
 
    show_time(std::cout, "1,000,000 int searches in ms", 1, [&]()
    {
-      for (size_t i = 0; i < 1000000; ++i)
+      for (size_t i = 0; i < SIZE; ++i)
          EXPECT_TRUE(hamt.contains(i));
+   });
+
+   show_time(std::cout, "1,000,000 int misses in ms", 1, [&]()
+   {
+      for (size_t i = 0; i < SIZE; ++i)
+         EXPECT_FALSE(hamt.contains(SIZE + i));
    });
 }
 
 TEST(HamtTest, perf_lots_of_collisions)
 {
    std::vector<std::pair<std::string, std::string>> inputs;
+   std::vector<std::pair<std::string, std::string>> misses;
    for (char c : "abcdefghijklmnopqrstuvwxyz")
+   {
       inputs.emplace_back(std::string(10, c), std::string(50, c));
+      misses.emplace_back(std::string(9, c), std::string(50, c));
+   }
 
    hash_array_mapped_trie<std::string, std::string> hamt;
+
    show_time(std::cout, "1,000,000 string inserts in ms", 1, [&]()
    {
       for (size_t i = 0; i < 1000000; ++i)
       {
          size_t p = i % inputs.size();
          hamt.insert(inputs[p].first, inputs[p].second);
+      }
+   });
+
+   show_time(std::cout, "1,000,000 string searches in ms", 1, [&]()
+   {
+      for (size_t i = 0; i < 1000000; ++i)
+      {
+         size_t p = i % inputs.size();
+         EXPECT_TRUE(hamt.contains(inputs[p].first));
+      }
+   });
+
+   show_time(std::cout, "1,000,000 string misses in ms", 1, [&]()
+   {
+      for (size_t i = 0; i < 1000000; ++i)
+      {
+         size_t p = i % misses.size();
+         EXPECT_FALSE(hamt.contains(misses[p].first));
       }
    });
 }
